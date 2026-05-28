ec5f91755a7a26c718bcdb7d6a58cb6144549691
Jonas Nicklas <jonas@jnicklas.com>
2026-05-28 10:11:58 +0200

    Fix dynamic controller import

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,9 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/components", under: "controllers"
+
+Dir.glob(Rails.root.join("app/components/**/*.js")).each do |file|
+  relative = Pathname.new(file).relative_path_from(Rails.root.join("app/components"))
+  name = relative.to_s.delete_suffix(".js").gsub("/", "_")
+  pin "controllers/#{name}", to: relative.to_s
+end
